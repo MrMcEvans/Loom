040b2a148f085dec1cb6b56834a277cc2701cb86
MrMcEvans <125924206+MrMcEvans@users.noreply.github.com>
2023-10-31 13:36:53 -0500

    d

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 
-const sequelize = 
+const sequelize = process.env.JAWSDB_URL?
+    new Sequelize(process.env.JAWSDB_URL) :
   new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: process.env.DB_HOST,
       dialect: 'mariadb',
